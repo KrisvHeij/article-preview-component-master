8da732e5607ed3e5c9f06b4083209bd3d677fd8c
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2025-06-29 14:30:53 +0200

    Added Timeout function to closeSocials function

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const socialsContainer = document.querySelector(".card-socials-container");
 let isSocialOpen = false;
 
 function showSocials() {
-  socialsContainer.classList.remove("close-socials");
+  socialsContainer.classList.remove("close-socials", "hidden");
   socialsContainer.classList.add("show-socials");
 }
 
 function closeSocials() {
   socialsContainer.classList.remove("show-socials");
   socialsContainer.classList.add("close-socials");
+  setTimeout(() => {
+    socialsContainer.classList.add("hidden");
+  }, 500)
 }
 
 shareBtn.addEventListener("click", () => {
